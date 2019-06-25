4a84f0ee4c7d122fa47f151dc115ea9e8c0d1c00
ItsukiMurayama <s1711472@s.tsukuba.ac.jp>
2019-06-26 05:42:00 +0900

    a

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     def index
-        @tasks = Task.all
+        @task = Task.all
     end
   
     def show
@@ -40,6 +40,11 @@ class TasksController < ApplicationController
     end
   
     def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+
+        flash[:success] = 'タスクが削除されました'
+        redirect_to tasks_path
     end
 end
 
